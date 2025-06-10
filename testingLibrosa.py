73ea943224d011f7779158c9778d8d89568a3ab9
myopian1 <leporacelara@gmail.com>
2025-06-10 15:18:38 -0300

    pruebaLibrosa2

--- testingLibrosa.py
+++ testingLibrosa.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 audio, sr = lb.load("audioPrueba.ogg")
-print(audio)
+print("\nSample rate de audioPrueba =", sr)
+print("Duración del audio en segundos =", len(audio) / sr)
 S = lb.stft(audio)
 S_db = lb.amplitude_to_db(np.abs(S), ref=np.max)
 
@@ -12,7 +13,7 @@ plt.figure(figsize=(10, 4))
 plt.imshow(S_db, aspect='auto', origin='lower', cmap='viridis')
 plt.colorbar(format='%+2.0f dB')
 plt.title('Spectrogram (dB)')
-plt.xlabel('Frames')
+plt.xlabel('Frames/samples')
 plt.ylabel('Frequency bins')
 plt.tight_layout()
 plt.show()
