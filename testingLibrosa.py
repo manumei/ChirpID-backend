fe8663a8ca8782a3755e86a083be8b6cdcc1fbf0
Lara Leporace <127157174+myopian1@users.noreply.github.com>
2025-06-11 13:32:20 -0300

    Update testingLibrosa.py

--- testingLibrosa.py
+++ testingLibrosa.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 audio, sr = lb.load("audioPrueba.ogg")
 print("\nSample rate de audioPrueba =", sr)
 print("Duración del audio en segundos =", len(audio) / sr)
-S = lb.stft(audio)
-S_db = lb.amplitude_to_db(np.abs(S), ref=np.max)
+ffourier = lb.stft(audio)
+S_db = lb.amplitude_to_db(np.abs(ffourier), ref=np.max)
 
+rms = librosa.feature.rms(S=ffourier, hop_length=1)  # root-mean-square value for each frame, hop-length es la cantidad de frames de audio que se usan para calcular cada valor de energía
 
 plt.figure(figsize=(10, 4))
 plt.imshow(S_db, aspect='auto', origin='lower', cmap='viridis')
