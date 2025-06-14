518a234dde775f2cd95d03b4a7e0469e865959c2
DragonWarrior💫 <supremedragonwarrior@gmail.com>
2025-06-14 17:50:17 -0300

    Refactor AudioProcessing notebook to improve spectrogram generation and update utility functions for better file handling

--- utils/util.py
+++ utils/util.py
@@ -6,3 +6,10 @@ def clean_dir(dest_dir):
     if os.path.exists(dest_dir):
         shutil.rmtree(dest_dir)
     os.makedirs(dest_dir, exist_ok=True)
+
+def count_files_in_dir(dir_path):
+    ''' Counts the number of files in a directory.'''
+    if not os.path.exists(dir_path):
+        return 0
+    return len([f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))])
+
