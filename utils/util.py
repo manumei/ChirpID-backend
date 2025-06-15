4476cab1dbf4e5287bd95209d82ae2bf5aea47e0
DragonWarrior💫 <supremedragonwarrior@gmail.com>
2025-06-15 12:37:16 -0300

    Refactor audio processing notebooks to improve clarity and add optional parameters for test audio saving

--- utils/util.py
+++ utils/util.py
@@ -3,6 +3,7 @@ import shutil
 
 def clean_dir(dest_dir):
     ''' Deletes the raw audio files in the dest_dir.'''
+    print(f"Resetting {dest_dir} directory...")
     if os.path.exists(dest_dir):
         shutil.rmtree(dest_dir)
     os.makedirs(dest_dir, exist_ok=True)
